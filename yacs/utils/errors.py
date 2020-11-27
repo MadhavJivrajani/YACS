48d5019b9fdaa51995d76c6e6c1291c5dc80cabf
Madhav Jivrajani <madhav.jiv@gmail.com>
2020-11-27 14:54:33 +0530

    update docs for utils and add type hinting for signal handler

--- yacs/utils/errors.py
+++ yacs/utils/errors.py
@@ -4,6 +4,6 @@ class ThreadTerminate(Exception):
 	"""Wrapper exception for when a thread terminates"""
 	pass
 
-def handle_thread_terminate(signum, frame):
+def handle_thread_terminate(signum: int, frame: object) -> None:
 	"""Handler function for graceful termination"""
 	raise ThreadTerminate
