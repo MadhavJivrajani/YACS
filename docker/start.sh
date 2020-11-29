17c9eade30a94c9f737040b78739450101f9a142
metonymic-smokey <ahuja.aditi@gmail.com>
2020-11-29 18:44:45 +0530

    shell script to run containers

---
+++ docker/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sched=$1    #scheduling policy
+n=$2        #no. of workers
+
+docker run -d --network host -e sched=$sched --name master aditiahuja/yacs_master
+
+sched=$1
+n=$2
+
+for (( c=1; c<=$n; c++ ))
+do  
+   echo "Enter ID and port for worker $c:"
+   read id p
+   cont_name=w$c
+   #echo $cont_name
+   docker run -d --network host -e id=$id -e port=$p --name $cont_name aditiahuja/yacs_worker
+
+done
+
+echo "Enter no. of requests:"
+read r
+
+docker exec -it master bash -c "python3 gen_requests.py $r"
+
+
+
